0e6ee69e7f3e147fc5d1b6c45083fbde4a9e0ec8
milliafsd <milliafsd1@gmail.com>
2026-03-10 01:53:52 +0500

    Create database.py

---
+++ database.py
@@ -0,0 +1,12 @@
+# database.py
+import mysql.connector
+from config import DB_HOST, DB_USER, DB_PASS, DB_NAME
+
+def get_connection():
+    conn = mysql.connector.connect(
+        host=DB_HOST,
+        user=DB_USER,
+        password=DB_PASS,
+        database=DB_NAME
+    )
+    return conn
