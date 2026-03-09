484ff365206a735fb626aafb1b9a9efa0ea00a71
milliafsd <milliafsd1@gmail.com>
2026-03-10 01:53:02 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+DB_HOST = "localhost"
+DB_USER = "millifsd_user"
+DB_PASS = "your_password"
+DB_NAME = "millifsd_madrasa"
+
+# WhatsApp API (trial)
+WHATSAPP_API_URL = "https://graph.facebook.com/v16.0/YOUR_PHONE_NUMBER_ID/messages"
+WHATSAPP_TOKEN = "YOUR_ACCESS_TOKEN"
